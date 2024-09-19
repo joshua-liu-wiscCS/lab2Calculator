19fa48365b9ee9287c3ece6727d836eb914ad174
joshua-liu-wiscCS <jzliu4@wisc.edu>
2024-09-19 17:50:00 -0500

    Milestone 2 Complete

--- app/src/main/java/com/cs407/lab2calculator/MainActivity.kt
+++ app/src/main/java/com/cs407/lab2calculator/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
