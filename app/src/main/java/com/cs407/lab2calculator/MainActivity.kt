91cf24ee2a6c814616f33c3cdcfb41ce338f8a86
joshua-liu-wiscCS <jzliu4@wisc.edu>
2024-09-19 17:49:15 -0500

    Milestone 2 Complete

--- app/src/main/java/com/cs407/lab2calculator/MainActivity.kt
+++ app/src/main/java/com/cs407/lab2calculator/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.cs407.lab2calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +20,57 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val firstInput = findViewById<EditText>(R.id.FirstNumber)
+        firstInput.inputType = InputType.TYPE_CLASS_NUMBER
+        val secondInput = findViewById<EditText>(R.id.SecondNumber)
+        secondInput.inputType = InputType.TYPE_CLASS_NUMBER
+
+        val addition = findViewById<Button>(R.id.addition)
+        addition.setOnClickListener{
+            val firstNumber = firstInput.text.toString().toInt()
+            val secondNumber = secondInput.text.toString().toInt()
+            val sum = firstNumber.toDouble() + secondNumber
+            val intent = Intent(this, CalculatorResult::class.java)
+            intent.putExtra("EXTRA_MESSAGE", sum.toString())
+            startActivity(intent)
+        }
+
+        val subtraction = findViewById<Button>(R.id.subtraction)
+        subtraction.setOnClickListener{
+            val firstNumber = firstInput.text.toString().toInt()
+            val secondNumber = secondInput.text.toString().toInt()
+            val difference = firstNumber.toDouble() - secondNumber
+            val intent = Intent(this, CalculatorResult::class.java)
+            intent.putExtra("EXTRA_MESSAGE", difference.toString())
+            startActivity(intent)
+        }
+
+        val multiplication = findViewById<Button>(R.id.multiplication)
+        multiplication.setOnClickListener{
+            val firstNumber = firstInput.text.toString().toInt()
+            val secondNumber = secondInput.text.toString().toInt()
+            val product = firstNumber.toDouble() * secondNumber
+            val intent = Intent(this, CalculatorResult::class.java)
+            intent.putExtra("EXTRA_MESSAGE", product.toString())
+            startActivity(intent)
+        }
+
+        val division = findViewById<Button>(R.id.division)
+        division.setOnClickListener{
+            val firstNumber = firstInput.text.toString().toInt()
+            val secondNumber = secondInput.text.toString().toInt()
+            if (secondNumber != 0) {
+                val quotient = "%.3f".format(firstNumber.toDouble()/secondNumber)
+                val intent = Intent(this, CalculatorResult::class.java)
+                intent.putExtra("EXTRA_MESSAGE", quotient)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, CalculatorResult::class.java)
+                intent.putExtra("EXTRA_MESSAGE", "Cannot divide by 0")
+                startActivity(intent)
+            }
         }
     }
 }
